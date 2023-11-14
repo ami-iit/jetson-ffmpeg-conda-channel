1c373e86ee6cfb8eeb0619f8795a9369aecfc291
Silvio Traversaro <silvio@traversaro.it>
2023-11-14 10:32:56 +0100

    Update build.sh

--- nvmpi/build.sh
+++ nvmpi/build.sh
@@ -4,7 +4,7 @@ LD_LIBRARY_PATH=$(pwd)/stubs:$LD_LIBRARY_PATH
 
 mkdir build && cd build
 
-cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
+cmake -GNinja ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_SHARED_LIBS=ON \
